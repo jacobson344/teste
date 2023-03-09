a752e8b86324008efdf975c9f7d3db555b08b4aa
jacobson344 <jacobson.sistemas@gmail.com>
2023-03-09 19:23:12 -0300

    Primeiro commit dos projetos das aulas.

---
+++ src/br/com/jacobsonsistemas/OlaMundo.java
@@ -0,0 +1,8 @@
+package br.com.jacobsonsistemas;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println(" Olá Mundo !" );
+        System.out.println(" Teste");
+    }
+}
