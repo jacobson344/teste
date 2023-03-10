1c4c74ce5604c1f9ed69cc35041a5981581e3af3
jacobson344 <jacobson.sistemas@gmail.com>
2023-03-09 23:29:15 -0300

    Atualizações Classe Operadores

--- src/br/com/jacobsonsistemas/Operadores.java
+++ src/br/com/jacobsonsistemas/Operadores.java
@@ -4,4 +4,17 @@ public class Operadores {
     public static void main(String[] args) {
         System.out.println(" Vamos começar a usar operadores ");
     }
+
 }
+
+/*
+Tipo de Operadores:
+
+1 - Operadores Lógicos;
+2 - Operadores Relacionais;
+3 - Operadores Unário;
+4 - Operadores Aritmético;
+5 - Operadores Shift;
+6 - operadores bit a bit (bitwise);
+7 - Operador Ternário;
+8 - Operador de Atribuição.*/
